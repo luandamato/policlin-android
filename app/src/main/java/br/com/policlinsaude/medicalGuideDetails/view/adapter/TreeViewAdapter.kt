3e8233ee11af1f332cf56bf269431468d6b5c556
Luan Damato <luandamato@gmail.com>
2026-08-24 12:08:08 -0300

    prompt 0

--- app/src/main/java/br/com/policlinsaude/medicalGuideDetails/view/adapter/TreeViewAdapter.kt
+++ app/src/main/java/br/com/policlinsaude/medicalGuideDetails/view/adapter/TreeViewAdapter.kt
@@ -3,49 +3,65 @@ package br.com.policlinsaude.medicalGuideDetails.view.adapter
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseExpandableListAdapter
+import android.widget.TextView
 
-class TreeViewAdapter: BaseExpandableListAdapter(){
+class TreeViewAdapter : BaseExpandableListAdapter() {
 
+    private val groups: MutableList<String> = mutableListOf()
+    private val children: MutableList<MutableList<String>> = mutableListOf()
 
-    override fun getGroup(p0: Int): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    fun updateData(groups: List<String>, children: List<List<String>>) {
+        this.groups.clear()
+        this.groups.addAll(groups)
+        this.children.clear()
+        this.children.addAll(children.map { it.toMutableList() })
+        notifyDataSetChanged()
     }
 
-    override fun isChildSelectable(p0: Int, p1: Int): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getGroupCount(): Int = groups.size
+
+    override fun getChildrenCount(groupPosition: Int): Int {
+        if (groupPosition !in groups.indices) return 0
+        return children.getOrNull(groupPosition)?.size ?: 0
     }
 
-    override fun hasStableIds(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getGroup(groupPosition: Int): Any = groups.getOrElse(groupPosition) { "" }
+
+    override fun getChild(groupPosition: Int, childPosition: Int): Any {
+        val groupChildren = children.getOrNull(groupPosition) ?: return ""
+        return groupChildren.getOrElse(childPosition) { "" }
     }
 
-    override fun getGroupView(p0: Int, p1: Boolean, p2: View?, p3: ViewGroup): View {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getGroupId(groupPosition: Int): Long = groupPosition.toLong()
+
+    override fun getChildId(groupPosition: Int, childPosition: Int): Long = childPosition.toLong()
+
+    override fun hasStableIds(): Boolean = false
+
+    override fun isChildSelectable(groupPosition: Int, childPosition: Int): Boolean = true
+
+    override fun getGroupView(
+        groupPosition: Int,
+        isExpanded: Boolean,
+        convertView: View?,
+        parent: ViewGroup
+    ): View {
+        val textView = (convertView as? TextView) ?: TextView(parent.context)
+        textView.text = getGroup(groupPosition).toString()
+        textView.setPadding(48, 24, 48, 24)
+        return textView
     }
 
-    override fun getChildrenCount(p0: Int): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getChildView(
+        groupPosition: Int,
+        childPosition: Int,
+        isLastChild: Boolean,
+        convertView: View?,
+        parent: ViewGroup
+    ): View {
+        val textView = (convertView as? TextView) ?: TextView(parent.context)
+        textView.text = getChild(groupPosition, childPosition).toString()
+        textView.setPadding(96, 16, 48, 16)
+        return textView
     }
-
-    override fun getChild(p0: Int, p1: Int): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getGroupId(p0: Int): Long {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getChildView(p0: Int, p1: Int, p2: Boolean, p3: View?, p4: ViewGroup): View {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getChildId(p0: Int, p1: Int): Long {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getGroupCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-
 }
